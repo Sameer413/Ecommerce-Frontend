aabcd80172f95fdf28119ad0d46c6afcc0046474
Sameer413 <92072016+Sameer413@users.noreply.github.com>
2023-07-09 15:47:47 +0530

    desktop footer completed

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,28 +1,79 @@
 import React from 'react'
 import { footer } from '../../data'
+import { BsBriefcaseFill } from 'react-icons/bs'
+import { MdStars, MdCardGiftcard, MdHelp, MdCopyright } from 'react-icons/md'
+import payment from '../../assets/payment.svg'
 
-const FooterOptions = ({ item }) => {
+const FooterOptions = ({ item, title }) => {
     return (
-        <div className="">{item}</div>
+        <div className="flex flex-col mx-5 ">
+            <span className="text-[#878787] text-base mb-2 font-bold mt-4">{title}</span>
+
+            {item.map((item, i) => (
+                <a
+                    key={i}
+                    href="#"
+                    className="text-sm font-normal m-1 hover:underline text-start"
+                >{item}</a>
+            ))}
+        </div>
+    )
+}
+
+const FooterDownOptions = ({ icon, title }) => {
+    return (
+        <div className="flex gap-2 items-center mx-8">
+            {icon} <span>{title}</span>
+        </div>
     )
 }
 
 
-
 const Footer = () => {
     return (
-        <footer className="bg-[#172337] w-full text-white">
-            <div className="w-[90%] mx-auto">
-                <div className="flex">
+        <footer className="bg-[#172337] w-full text-white box-border">
+            <div className="w-[90%] mx-auto p-10">
+                <div className="flex flex-wrap">
 
-                    {footer.about.map((item, i) => (
-                        <FooterOptions key={i} item={item} />
-                    ))}
+                    <div className="flex-[2] flex flex-wrap ">
+                        <FooterOptions title={footer.about.title} item={footer.about.info} />
+                        <FooterOptions title={footer.help.title} item={footer.help.info} />
+                        <FooterOptions title={footer.consumerPolicy.title} item={footer.consumerPolicy.info} />
+                        <FooterOptions title={footer.social.title} item={footer.social.info} />
+                    </div>
 
-                    <div className="">b</div>
+
+                    <div className="flex mx-5 flex-1">
+                        <div className="flex flex-col px-6 text-sm border-l">
+                            <span className="text-[#878787] text-base mb-2 font-bold mt-4">Mail Us</span>
+                            <span>{footer.mailUs}</span>
+                        </div>
+                        {/*  */}
+                        <div className="flex flex-col px-6 text-sm">
+                            <span className="text-[#878787] text-base mb-2 font-bold mt-4">Address</span>
+                            <span>{footer.address.add}</span>
+                            <span>{footer.address.cin}</span>
+                            <span>{footer.address.telephone}</span>
+                        </div>
+                    </div>
                 </div>
                 {/* down */}
-                <div className=""></div>
+                <div className="mt-7 border-t flex items-center">
+                    <div className="py-3 flex">
+                        <FooterDownOptions title={"Become a Seller"} icon={<BsBriefcaseFill color='yellow' />} />
+                        <FooterDownOptions title={"Advertise"} icon={<MdStars color='yellow' />} />
+                        <FooterDownOptions title={"Gift Cards"} icon={<MdCardGiftcard color='yellow' />} />
+                        <FooterDownOptions title={"help"} icon={<MdHelp color='yellow' />} />
+                        <FooterDownOptions title={"2007-2023 Sameer Nimje"} icon={<MdCopyright />} />
+                    </div>
+                    <div className="py-3">
+                        <img
+                            src={payment}
+                            alt=""
+                            className=""
+                        />
+                    </div>
+                </div>
             </div>
         </footer>
     )
--- src/data.js
+++ src/data.js
@@ -1,24 +1,28 @@
 export const footer = {
-    about: [
-        "Contact Us", "About Us", "Careers", "Flipkart Stories", "Press", "Flipkart Wholesale", "Corporate Information",
-    ],
-    help: [
-        "Payments", "Shipping", "Cancellation & Returns", "FAQ", "Report Infringement",
-    ],
-    consumerPolicy: [
-        "Return Policy", "Terms Of Use", "Security", "Privacy", "Sitemap", "Grievance Redressal", "EPR Compliance",
-    ],
-    social: [
-        "Facebook", "Twitter", "YouTube"
-    ],
+    about: {
+        title: "About",
+        info: ["Contact Us", "About Us", "Careers", "Flipkart Stories", "Press", "Flipkart Wholesale", "Corporate Information",]
+    },
+    help: {
+        title: "Help",
+        info: ["Payments", "Shipping", "Cancellation & Returns", "FAQ", "Report Infringement",]
+    },
+    consumerPolicy: {
+        title: "Consumer Policy",
+        info: ["Return Policy", "Terms Of Use", "Security", "Privacy", "Sitemap", "Grievance Redressal", "EPR Compliance",]
+    },
+    social: {
+        title: "Socials",
+        info: ["Facebook", "Twitter", "YouTube"]
+    },
     mailUs: [
         "Flipkart Internet Private Limited, Buildings Alyssa, Begonia & Clove Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village, Bengaluru, 560103, Karnataka, India"
     ],
-    address: [
-        {
-            add: "Flipkart Internet Private Limited, Buildings Alyssa, Begonia & Clove Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village, Bengaluru, 560103, Karnataka, India",
-            cin: "U51109KA2012PTC066107",
-            telephone: "044-45614700"
-        }
-    ]
+    address: {
+
+        add: "Flipkart Internet Private Limited, Buildings Alyssa, Begonia & Clove Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village, Bengaluru, 560103, Karnataka, India",
+        cin: "U51109KA2012PTC066107",
+        telephone: "044-45614700"
+
+    }
 }
